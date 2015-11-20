0401e61c2cac1613b5f6f77cddf30e7bc954fd68
Thi <t@thi.im>
2015-11-20 13:51:55 +0900

    Add buttons

--- Notie/NotieView.swift
+++ Notie/NotieView.swift
@@ -8,7 +8,7 @@ public class NotieView: UIView {
     public var rightButtonTitle: String = "No"
 
     public init(message: String, leftButtonTitle: String?, rightButtonTitle: String?) {
-        super.init(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 80))
+        super.init(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 124))
         self.message = message
 
         if leftButtonTitle != nil {
@@ -20,19 +20,33 @@ public class NotieView: UIView {
         }
 
         self.configureView()
+        self.configureLeftButton(leftButtonTitle)
+        self.configureRightButton(rightButtonTitle)
     }
 
     func configureView() {
         self.backgroundColor = UIColor(hexString: "#5887cf")
-        let frame = self.frame
-        let messageLabel = UILabel(frame: frame)
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 80))
         messageLabel.centerX = self.centerX
         messageLabel.centerY = messageLabel.centerY + screenStatusBarHeight / 2
         messageLabel.text = self.message
         messageLabel.textAlignment = .Center
         messageLabel.textColor = UIColor.whiteColor()
         self.addSubview(messageLabel)
+    }
 
+    func configureLeftButton(title: String?) {
+        let button = UIButton(frame: CGRect(x: 0, y: 80, width: screenWidth / 2, height: 44))
+        button.backgroundColor = UIColor(hexString: "#75b760")
+        button.setTitle(title, forState: .Normal)
+        self.addSubview(button)
+    }
+
+    func configureRightButton(title: String?) {
+        let button = UIButton(frame: CGRect(x: screenWidth / 2, y: 80, width: screenWidth / 2, height: 44))
+        button.backgroundColor = UIColor(hexString: "#d27866")
+        button.setTitle(title, forState: .Normal)
+        self.addSubview(button)
     }
 
     required public init(coder aDecoder: NSCoder) {
