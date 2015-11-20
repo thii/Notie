cd29d2b6b2128871bee35f40f7f91144afe0527b
Thi <t@thi.im>
2015-11-20 15:28:11 +0900

    Add button handlers

--- Notie/Notie.swift
+++ Notie/Notie.swift
@@ -3,8 +3,10 @@ import UIKit
 public class Notie: NSObject {
     public static let sharedNotie = Notie()
 
-    public func show(view: UIView, message: String, leftButtonTitle: String?, rightButtonTitle: String?) {
+    public func show(view: UIView, message: String, leftButtonTitle: String?, rightButtonTitle: String?, leftButtonBlock: (() -> ())? = nil, rightButtonBlock: (() -> ())? = nil) {
         let notieView = NotieView(message: message, leftButtonTitle: leftButtonTitle, rightButtonTitle: rightButtonTitle)
+        notieView.leftButtonBlock = leftButtonBlock
+        notieView.rightButtonBlock = rightButtonBlock
         view.addSubview(notieView)
 
         UIView.animateWithDuration(0.03) { () -> Void in
--- Notie/NotieView.swift
+++ Notie/NotieView.swift
@@ -7,6 +7,9 @@ public class NotieView: UIView {
     public var leftButtonTitle: String = "Yes"
     public var rightButtonTitle: String = "No"
 
+    public var leftButtonBlock: (() -> ())?
+    public var rightButtonBlock: (() -> ())?
+
     public init(message: String, leftButtonTitle: String?, rightButtonTitle: String?) {
         super.init(frame: CGRect(x: 0, y: -124, width: screenWidth, height: 124))
         self.message = message
@@ -39,6 +42,7 @@ public class NotieView: UIView {
         let button = UIButton(frame: CGRect(x: 0, y: 80, width: screenWidth / 2, height: 44))
         button.backgroundColor = UIColor(hexString: "#75b760")
         button.setTitle(title, forState: .Normal)
+        button.addTarget(self, action: "leftButtonDidTap", forControlEvents: .TouchUpInside)
         self.addSubview(button)
     }
 
@@ -46,10 +50,21 @@ public class NotieView: UIView {
         let button = UIButton(frame: CGRect(x: screenWidth / 2, y: 80, width: screenWidth / 2, height: 44))
         button.backgroundColor = UIColor(hexString: "#d27866")
         button.setTitle(title, forState: .Normal)
+        button.addTarget(self, action: "rightButtonDidTap", forControlEvents: .TouchUpInside)
         self.addSubview(button)
     }
 
     required public init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
+    }
+
+    // MARK: Button Handlers
+
+    func leftButtonDidTap() {
+        print("Left")
+    }
+
+    func rightButtonDidTap() {
+        print("Right")
     }
 }
