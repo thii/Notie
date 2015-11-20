50fb5f1db13ec06bbb6e12c6749918ad50159466
Thi <t@thi.im>
2015-11-20 18:57:04 +0900

    Add animationDuration variable

--- Notie/Notie.swift
+++ Notie/Notie.swift
@@ -7,6 +7,7 @@ let notieViewHeight = messageViewHeight + buttonHeight
 public class Notie: NSObject {
     public static let sharedNotie = Notie()
     public var currentNotieView: NotieView?
+    public var animationDuration: NSTimeInterval = 0.4
 
     public func show(view: UIView, message: String, withInputField: Bool, placeholder: String?, leftButtonTitle: String?, rightButtonTitle: String?, leftButtonBlock: (() -> ())? = nil, rightButtonBlock: (() -> ())? = nil) {
         self.currentNotieView = NotieView(message: message, withInputField: withInputField, placeholder: placeholder, leftButtonTitle: leftButtonTitle, rightButtonTitle: rightButtonTitle)
@@ -14,13 +15,13 @@ public class Notie: NSObject {
         self.currentNotieView!.rightButtonBlock = rightButtonBlock
         view.addSubview(self.currentNotieView!)
 
-        UIView.animateWithDuration(0.4) { () -> Void in
+        UIView.animateWithDuration(animationDuration) { () -> Void in
             self.currentNotieView!.centerY += notieViewHeight
         }
     }
 
     public func dismiss() {
-        UIView.animateWithDuration(0.4, animations: { () -> Void in
+        UIView.animateWithDuration(animationDuration, animations: { () -> Void in
             self.currentNotieView!.centerY -= notieViewHeight
             }) { (_) -> Void in
                 self.currentNotieView?.removeFromSuperview()
