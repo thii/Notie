fed02d012601ae6bad240288e59504fad4c207b2
Thi <t@thi.im>
2015-11-20 12:14:48 +0900

    $ gibo swift > .gitignore

---
+++ Notie/Notie.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Notie.
+FOUNDATION_EXPORT double NotieVersionNumber;
+
+//! Project version string for Notie.
+FOUNDATION_EXPORT const unsigned char NotieVersionString[];
