ef3731be59335fe916a7b394dce6b85b61e1268d
Thi <t@thi.im>
2015-11-21 20:13:25 +0900

    Temporarily remove example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -14,29 +14,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showNotie(sender: AnyObject) {
-        let notie = Notie.sharedNotie
-        notie.show(self.view, message: "Are you sure you want to do that?", withInputField: false, placeholder: nil, leftButtonTitle: "Yes", rightButtonTitle: "No", leftButtonBlock: { () -> () in
-            // Left button handler
-            notie.dismiss()
-            }) { () -> () in
-            // Right button handler
-            notie.dismiss()
-        }
     }
 
     @IBAction func showInputNotie(sender: AnyObject) {
-        let notie = Notie.sharedNotie
-        notie.show(self.view, message: "Please enter your email address", withInputField: true, placeholder: "email@example.com", leftButtonTitle: "Submit", rightButtonTitle: "Cancel", leftButtonBlock: { () -> () in
-            // Left button handler
-            notie.dismiss()
-            }) { () -> () in
-            // Right button handler
-            notie.dismiss()
-        }
     }
 
     @IBAction func hideNotie(sender: AnyObject) {
-        let notie = Notie.sharedNotie
-        notie.dismiss()
     }
 }
