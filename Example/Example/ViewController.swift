1b3fed4b942a6e956aeec7ac8f0742780d86a94f
Thi <t@thi.im>
2015-11-21 21:01:46 +0900

    Update example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -14,11 +14,31 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showNotie(sender: AnyObject) {
+        let notie = Notie(view: self.view, title: "", message: "Are you sure you want to do that?", style: .Confirm)
+
+        notie.leftButtonAction = {
+            notie.dismiss()
+        }
+
+        notie.rightButtonAction = {
+            notie.dismiss()
+        }
+
+        notie.show()
     }
 
     @IBAction func showInputNotie(sender: AnyObject) {
+        let notie = Notie(view: self.view, title: "", message: "Please enter your email address", style: .Input)
+
+        notie.leftButtonAction = {
+            notie.dismiss()
+        }
+
+        notie.rightButtonAction = {
+            notie.dismiss()
+        }
+        
+        notie.show()
     }
 
-    @IBAction func hideNotie(sender: AnyObject) {
-    }
 }
