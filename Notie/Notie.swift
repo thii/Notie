4ed3dd6baec319838121a24dde0e0e850af0794f
Thi <t@thi.im>
2015-12-03 22:54:27 +0900

    100% documentation.

--- Notie/Notie.swift
+++ Notie/Notie.swift
@@ -92,6 +92,7 @@ public class Notie : UIView {
         super.init(frame: CGRectZero)
     }
 
+    /// This is required for a subclass of NSCoding. Just don't care about it.
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
