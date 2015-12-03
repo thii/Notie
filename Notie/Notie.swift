36be96f21211d51095a6821315d158167546f0bc
Thi <t@thi.im>
2015-12-03 22:59:16 +0900

    Documentation.

--- Notie/Notie.swift
+++ Notie/Notie.swift
@@ -92,7 +92,7 @@ public class Notie : UIView {
         super.init(frame: CGRectZero)
     }
 
-    /// This is required for a subclass of NSCoding. Just don't care about it.
+    /// This is required for classes conform to NSCoding protocol. Just don't care about it.
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Notie/NotieStyle.swift
+++ Notie/NotieStyle.swift
@@ -1,11 +1,12 @@
 import UIKit
 
-/// The `style` of the notification.
-/// - Confirm: The notification will include a message view and two confirm buttons. 
-/// - Input: The notification will include a message view, an input text field and two confirm buttons.
+/// The preferred style of the notification.
 @available(iOS 9.0, *)
 public enum NotieStyle : Int {
 
+    /// The notification will include a message view and two confirm buttons.
     case Confirm
+
+    /// The notification will include a message view, an input text field and two confirm buttons.
     case Input
 }
