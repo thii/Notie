d1820dffc5ec73ea4f4afe4fd5b452beb52d73ee
Keaton Burleson <keaton.burleson@me.com>
2016-04-07 11:29:15 -0500

    * added demonstration * added a button count * added a fetch text option

---
+++ NotieDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  NotieDemo
+//
+//  Created by Keaton Burleson on 4/7/16.
+//
+//
+
+import UIKit
+import Notie
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let notie = Notie.init(view: self.view, message: "Hello", style: .Input)
+        notie.buttonCount = Notie.buttons.single
+        notie.leftButtonAction = {
+            print(notie.getText())
+        }
+        notie.show()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
